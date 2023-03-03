04c6a0ce735a7cbabf6c3811b84ac37caeecdfd4
Oleh Stashchyshyn <o.s.dubbie@gmail.com>
2023-03-03 13:14:55 +0200

    homework 2/2 ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkUp = createPhotosMarkUpGallery(galleryItems);
+
+function createPhotosMarkUpGallery(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}">
+  </a></li>`})
+  .join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
